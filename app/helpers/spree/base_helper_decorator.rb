232fc76317fb8ed5f91bf695739c32d38370421d
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2015-04-02 22:25:05 +0300

    Compatibility with spree 3.0.0

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -2,15 +2,15 @@ Spree::BaseHelper.class_eval do
   def display_original_price(product_or_variant)
     product_or_variant.original_price_in(_current_currency).display_price.to_html
   end
-  
+
   def display_discount_percent(product_or_variant, append_text = 'Off')
     discount = product_or_variant.discount_percent_in _current_currency
-    
+
     if discount > 0
       return "#{number_to_percentage(discount, precision: 0).to_html} #{append_text}"
     else
       return ''
-    end 
+    end
   end
 
   # Check if a sale is the current sale for a product, returns true or false
@@ -19,7 +19,7 @@ Spree::BaseHelper.class_eval do
   end
 
   def supported_currencies_for_sale_price
-    supported_currencies || [ _current_currency ]
+    (defined?(supported_currencies) ? supported_currencies : nil) || [ _current_currency ]
   end
 
   private
