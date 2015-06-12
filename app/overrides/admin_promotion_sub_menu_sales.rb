e9d4801de228c7868d7df2242de99b7bcfc6d6ff
Rene Schneider <renews@oqvestir.com.br>
2015-06-12 14:32:57 -0300

    Adicionando itens no menu item.

---
+++ app/overrides/admin_promotion_sub_menu_sales.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => "spree/admin/shared/sub_menu/_promotion",
+                     :name => "admin_promotion_sub_menu_sales",
+                     :insert_bottom => "[data-hook='admin_promotion_sub_tabs']",
+                     :text => "<%= tab :sales, url: 'sales', icon: 'icon-th-large' %>",
+                     :disabled => false)
