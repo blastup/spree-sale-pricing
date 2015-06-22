cd5e5d32f877ad7202fa6f76d06061b72ce6a3a2
Ivan Yukio <ivan.birkman@oqvestir.com.br>
2015-06-22 17:01:00 -0300

    Arruma rota para página de sale prices

--- app/overrides/admin_promotion_sub_menu_sales.rb
+++ app/overrides/admin_promotion_sub_menu_sales.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/sub_menu/_promotion",
                      :name => "admin_promotion_sub_menu_sales",
                      :insert_bottom => "[data-hook='admin_promotion_sub_tabs']",
-                     :text => "<%= tab :sales, url: 'sale_promotions', icon: 'icon-th-large' %>",
+                     :text => "<%= tab :sales, url: '/admin/sale_promotions', icon: 'icon-th-large' %>",
                      :disabled => false)
