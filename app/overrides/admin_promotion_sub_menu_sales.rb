4ca6ad6f479d55a48bb60f9c50f3e826e52cae95
Ivan Yukio <ivan.birkman@oqvestir.com.br>
2015-06-23 10:49:22 -0300

    Arruma tag para botão do menu

--- app/overrides/admin_promotion_sub_menu_sales.rb
+++ app/overrides/admin_promotion_sub_menu_sales.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/sub_menu/_promotion",
                      :name => "admin_promotion_sub_menu_sales",
                      :insert_bottom => "[data-hook='admin_promotion_sub_tabs']",
-                     :text => "<%= tab :sales, url: '/admin/sale_promotions', icon: 'icon-th-large' %>",
+                     :text => "<li class='sidebar-menu-item'><%= link_to 'Sales', '/admin/sale_promotions', class: 'icon-th-large' %></li>",
                      :disabled => false)
